208696d19fc10d5d744014bbb234655067ac4f6a
omkargudekar <omkargudekar@gmail.com>
2014-12-03 01:14:17 -0800

    Version 2

--- src/java-rest-server/src/com/crimeanalysis/app/db/dao/Test.java
+++ src/java-rest-server/src/com/crimeanalysis/app/db/dao/Test.java
@@ -15,7 +15,7 @@ public class Test
 	public static void main(String[] args)
 	{
 		AnalysisRequest an=new AnalysisRequest();
-		an.setRadius(0.25);
+		an.setRadius(50000);
 		an.setLat(40.747257);
 		an.setLon(-111.888683);
 		an.setStartDate(DateUtil.convert("01-11-2001"));
@@ -30,7 +30,7 @@ public class Test
 		//ArrayList<ByTimeAnalysis> result=dao.getAnalysis(an);
 		
 		ArrayList<ByTypeAnalysis> result=dao.getAnalysis(an);
-		System.out.println(result);
+		System.out.println(result.size());
 	}
 
 }
